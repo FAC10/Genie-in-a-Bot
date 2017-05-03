c39f945b7ceded79ba7f4b32bc0c3e91b196dd0d
pbywater <philippabywater@gmail.com>
2017-05-03 16:18:02 +0100

    reverting

--- webhook.js
+++ webhook.js
@@ -62,87 +62,74 @@ app.post('/webhook', function (req, res) {
   }
 });
 
-var request = app.textRequest('Hello friend', {
-    sessionId: 'mp-bot'
-});
 
-request.on('response', function(response) {
-    console.log(response);
-});
+function receivedMessage(event) {
+  var senderID = event.sender.id;
+    var recipientID = event.recipient.id;
+    var timeOfMessage = event.timestamp;
+    var message = event.message;
 
-request.on('error', function(error) {
-    console.log(error);
-});
+    console.log("Received message for user %d and page %d at %d with message:",
+      senderID, recipientID, timeOfMessage);
+    console.log(JSON.stringify(message));
 
-request.end();
+    var messageId = message.mid;
 
-// function receivedMessage(event) {
-//   var senderID = event.sender.id;
-//     var recipientID = event.recipient.id;
-//     var timeOfMessage = event.timestamp;
-//     var message = event.message;
-//
-//     console.log("Received message for user %d and page %d at %d with message:",
-//       senderID, recipientID, timeOfMessage);
-//     console.log(JSON.stringify(message));
-//
-//     var messageId = message.mid;
-//
-//     var messageText = message.text;
-//     var messageAttachments = message.attachments;
-//
-//     if (messageText) {
-//
-//       // If we receive a text message, check to see if it matches a keyword
-//       // and send back the example. Otherwise, just echo the text we received.
-//       switch (messageText) {
-//         case 'generic':
-//           sendGenericMessage(senderID);
-//           break;
-//
-//         default:
-//           sendTextMessage(senderID, messageText);
-//       }
-//     } else if (messageAttachments) {
-//       sendTextMessage(senderID, "Message with attachment received");
-//     }
-// }
-//
-// function sendGenericMessage(recipientId, messageText) {
-// console.log('sending generic message');
-// }
-//
-// function sendTextMessage(recipientId, messageText) {
-//   // var messageData = {
-//   //   recipient: {
-//   //     id: recipientId
-//   //   },
-//   //   message: {
-//   //     text: messageText
-//   //   }
-//   // };
-//
-//   callSendAPI(messageData);
-// }
-//
-// function callSendAPI(messageData) {
-//   request({
-//     uri: 'https://graph.facebook.com/v2.6/me/messages',
-//     qs: { access_token: process.env.PAGE_ACCESS_TOKEN },
-//     method: 'POST',
-//     json: messageData
-//
-//   }, function (error, response, body) {
-//     if (!error && response.statusCode == 200) {
-//       var recipientId = body.recipient_id;
-//       var messageId = body.message_id;
-//
-//       console.log("Successfully sent generic message with id %s to recipient %s",
-//         messageId, recipientId);
-//     } else {
-//       console.error("Unable to send message.");
-//       console.error(response);
-//       console.error(error);
-//     }
-//   });
-// }
+    var messageText = message.text;
+    var messageAttachments = message.attachments;
+
+    if (messageText) {
+
+      // If we receive a text message, check to see if it matches a keyword
+      // and send back the example. Otherwise, just echo the text we received.
+      switch (messageText) {
+        case 'generic':
+          sendGenericMessage(senderID);
+          break;
+
+        default:
+          sendTextMessage(senderID, messageText);
+      }
+    } else if (messageAttachments) {
+      sendTextMessage(senderID, "Message with attachment received");
+    }
+}
+
+function sendGenericMessage(recipientId, messageText) {
+console.log('sending generic message');
+}
+
+function sendTextMessage(recipientId, messageText) {
+  // var messageData = {
+  //   recipient: {
+  //     id: recipientId
+  //   },
+  //   message: {
+  //     text: messageText
+  //   }
+  // };
+
+  callSendAPI(messageData);
+}
+
+function callSendAPI(messageData) {
+  request({
+    uri: 'https://graph.facebook.com/v2.6/me/messages',
+    qs: { access_token: process.env.PAGE_ACCESS_TOKEN },
+    method: 'POST',
+    json: messageData
+
+  }, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      var recipientId = body.recipient_id;
+      var messageId = body.message_id;
+
+      console.log("Successfully sent generic message with id %s to recipient %s",
+        messageId, recipientId);
+    } else {
+      console.error("Unable to send message.");
+      console.error(response);
+      console.error(error);
+    }
+  });
+}
