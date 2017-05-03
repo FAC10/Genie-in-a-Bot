b65e64c0a3608b16ef87af358f6395633ff6ee2b
pbywater <philippabywater@gmail.com>
2017-05-03 16:27:29 +0100

    testing apiai

--- webhook.js
+++ webhook.js
@@ -4,7 +4,7 @@ const app = express();
 const request = require('request');
 require('env2')('./config.env');
 const apiai = require('apiai');
-const app = apiai(process.env.APIAI_CLIENT);
+const apiai_app = apiai(process.env.APIAI_CLIENT);
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -30,106 +30,122 @@ app.get('/webhook', function(req, res) {
   }
 });
 
-//Listen for messages from user
-
-app.post('/webhook', function (req, res) {
-  var data = req.body;
-
-  // Make sure this is a page subscription
-  if (data.object === 'page') {
-
-    // Iterate over each entry - there may be multiple if batched
-    data.entry.forEach(function(entry) {
-      var pageID = entry.id;
-      var timeOfEvent = entry.time;
-
-      // Iterate over each messaging event
-      entry.messaging.forEach(function(event) {
-        if (event.message) {
-          receivedMessage(event);
-        } else {
-          console.log("Webhook received unknown event: ", event);
-        }
-      });
-    });
-
-    // Assume all went well.
-    //
-    // You must send back a 200, within 20 seconds, to let us know
-    // you've successfully received the callback. Otherwise, the request
-    // will time out and we will keep trying to resend.
-    res.sendStatus(200);
-  }
+var apiai_request = apiai_app.textRequest('hi friend', {
+    sessionId: 'mp-bot'
 });
 
+apiai_request.on('response', function(response) {
+    console.log(response);
+});
 
-function receivedMessage(event) {
-  var senderID = event.sender.id;
-    var recipientID = event.recipient.id;
-    var timeOfMessage = event.timestamp;
-    var message = event.message;
+apiai_request.on('error', function(error) {
+    console.log(error);
+});
 
-    console.log("Received message for user %d and page %d at %d with message:",
-      senderID, recipientID, timeOfMessage);
-    console.log(JSON.stringify(message));
+apiai_request.end();
 
-    var messageId = message.mid;
 
-    var messageText = message.text;
-    var messageAttachments = message.attachments;
 
-    if (messageText) {
+//Listen for messages from user
 
-      // If we receive a text message, check to see if it matches a keyword
-      // and send back the example. Otherwise, just echo the text we received.
-      switch (messageText) {
-        case 'generic':
-          sendGenericMessage(senderID);
-          break;
-
-        default:
-          sendTextMessage(senderID, messageText);
-      }
-    } else if (messageAttachments) {
-      sendTextMessage(senderID, "Message with attachment received");
-    }
-}
-
-function sendGenericMessage(recipientId, messageText) {
-console.log('sending generic message');
-}
-
-function sendTextMessage(recipientId, messageText) {
-  // var messageData = {
-  //   recipient: {
-  //     id: recipientId
-  //   },
-  //   message: {
-  //     text: messageText
-  //   }
-  // };
-
-  callSendAPI(messageData);
-}
-
-function callSendAPI(messageData) {
-  request({
-    uri: 'https://graph.facebook.com/v2.6/me/messages',
-    qs: { access_token: process.env.PAGE_ACCESS_TOKEN },
-    method: 'POST',
-    json: messageData
-
-  }, function (error, response, body) {
-    if (!error && response.statusCode == 200) {
-      var recipientId = body.recipient_id;
-      var messageId = body.message_id;
-
-      console.log("Successfully sent generic message with id %s to recipient %s",
-        messageId, recipientId);
-    } else {
-      console.error("Unable to send message.");
-      console.error(response);
-      console.error(error);
-    }
-  });
-}
+// app.post('/webhook', function (req, res) {
+//   var data = req.body;
+//
+//   // Make sure this is a page subscription
+//   if (data.object === 'page') {
+//
+//     // Iterate over each entry - there may be multiple if batched
+//     data.entry.forEach(function(entry) {
+//       var pageID = entry.id;
+//       var timeOfEvent = entry.time;
+//
+//       // Iterate over each messaging event
+//       entry.messaging.forEach(function(event) {
+//         if (event.message) {
+//           receivedMessage(event);
+//         } else {
+//           console.log("Webhook received unknown event: ", event);
+//         }
+//       });
+//     });
+//
+//     // Assume all went well.
+//     //
+//     // You must send back a 200, within 20 seconds, to let us know
+//     // you've successfully received the callback. Otherwise, the request
+//     // will time out and we will keep trying to resend.
+//     res.sendStatus(200);
+//   }
+// });
+//
+//
+// function receivedMessage(event) {
+//   var senderID = event.sender.id;
+//     var recipientID = event.recipient.id;
+//     var timeOfMessage = event.timestamp;
+//     var message = event.message;
+//
+//     console.log("Received message for user %d and page %d at %d with message:",
+//       senderID, recipientID, timeOfMessage);
+//     console.log(JSON.stringify(message));
+//
+//     var messageId = message.mid;
+//
+//     var messageText = message.text;
+//     var messageAttachments = message.attachments;
+//
+//     if (messageText) {
+//
+//       // If we receive a text message, check to see if it matches a keyword
+//       // and send back the example. Otherwise, just echo the text we received.
+//       switch (messageText) {
+//         case 'generic':
+//           sendGenericMessage(senderID);
+//           break;
+//
+//         default:
+//           sendTextMessage(senderID, messageText);
+//       }
+//     } else if (messageAttachments) {
+//       sendTextMessage(senderID, "Message with attachment received");
+//     }
+// }
+//
+// function sendGenericMessage(recipientId, messageText) {
+// console.log('sending generic message');
+// }
+//
+// function sendTextMessage(recipientId, messageText) {
+//   // var messageData = {
+//   //   recipient: {
+//   //     id: recipientId
+//   //   },
+//   //   message: {
+//   //     text: messageText
+//   //   }
+//   // };
+//
+//   callSendAPI(messageData);
+// }
+//
+// function callSendAPI(messageData) {
+//   request({
+//     uri: 'https://graph.facebook.com/v2.6/me/messages',
+//     qs: { access_token: process.env.PAGE_ACCESS_TOKEN },
+//     method: 'POST',
+//     json: messageData
+//
+//   }, function (error, response, body) {
+//     if (!error && response.statusCode == 200) {
+//       var recipientId = body.recipient_id;
+//       var messageId = body.message_id;
+//
+//       console.log("Successfully sent generic message with id %s to recipient %s",
+//         messageId, recipientId);
+//     } else {
+//       console.error("Unable to send message.");
+//       console.error(response);
+//       console.error(error);
+//     }
+//   });
+// }
