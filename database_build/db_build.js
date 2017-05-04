91b90a254adb8810e6ebe3896c3f2b5bc30f5b0c
ConchobarMacNessa <alice.binghamcarr@gmail.com>
2017-05-04 12:12:10 +0100

    added logic for second database added the logic to create our second database, db_build_votes.sql We have run into difficultiies with this file, and are currently waiting for a method to convert it from mysql to postgres. relates #17

--- database_build/db_build.js
+++ database_build/db_build.js
@@ -2,9 +2,18 @@ const fs = require('fs');
 const path = require('path');
 const connect = require('./../src/database/db_connect');
 
-const build = fs.readFileSync(path.join(__dirname, 'db_build.sql'), 'utf8');
+const buildUsers = fs.readFileSync(path.join(__dirname, 'db_build.sql'), 'utf8');
+// const buildVotes = fs.readFileSync(path.join(__dirname, 'db_build_votes.sql'), 'utf8');
 
-connect.query(build, (err, res) => {
+connect.query(buildUsers, (err, res) => {
   if (err) throw err;
-  console.log('db build successful');
+  console.log('Users table built successfully');
 });
+
+
+// Below to be used when building our MPvotes database
+
+// connect.query(buildVotes, (err, res) => {
+//   if (err) console.log(err);
+//   console.log('Voting tables built successfully');
+// });
