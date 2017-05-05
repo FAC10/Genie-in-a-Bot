0338afb6fa4d0575a3b3d3e35a04b7e22c38c6ed
ConchobarMacNessa <alice.binghamcarr@gmail.com>
2017-05-05 16:34:36 +0100

    changing res.rows relates #31

--- src/database/get_data.js
+++ src/database/get_data.js
@@ -8,8 +8,8 @@ get.firstName = (facebookId, callback) => connect.query('SELECT firstname FROM u
   }
   // console.log('resname is ', res.Result.rows.firstname);
   // console.log('res.Result.rows ', res.Result.rows);
-  console.log('res.rows is ', res.rows);
-  return callback(null, res.rows.firstname);
+  console.log('res.rows is ', res.rows[0].firstname);
+  return callback(null, res.rows[0].firstname);
 });
 
 module.exports = get;
