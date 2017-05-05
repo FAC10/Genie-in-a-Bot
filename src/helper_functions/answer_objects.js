ea7539b7baf044d3741ccef7fc2c25dc4c4aa58e
ConchobarMacNessa <alice.binghamcarr@gmail.com>
2017-05-05 16:06:52 +0100

    created function to get name from database called this function in findlocalreply.js relates #31

--- src/helper_functions/answer_objects.js
+++ src/helper_functions/answer_objects.js
@@ -1,7 +1,7 @@
 
-const answer_objects= {
+const answer_objects = {
 
-  location_message:{
+  location_message: {
     text: 'Type your postcode or send me your location to get started :)',
     quick_replies: [
     { content_type: 'location' },
@@ -9,78 +9,74 @@ const answer_objects= {
   },
 
   FACEBOOK_WELCOME: {
-    attachment:{
-      type:'template',
+    attachment: {
+      type: 'template',
       payload: {
-        template_type:'button',
-        text:'Hey [name], I\'m your personal assistant in the run up to the General Elections! I can help you find out about the candidates standing in your area, what the parties are offering and more!',
-        buttons:[
+        template_type: 'button',
+        text: `Hey ${firstName}, I\'m your personal assistant in the run up to the General Elections! I can help you find out about the candidates standing in your area, what the parties are offering and more!`,
+        buttons: [
           {
-            type:'postback',
-            title:'Candidates',
-            payload:'Candidates'
+            type: 'postback',
+            title: 'Candidates',
+            payload: 'Candidates',
           },
           {
-            type:'postback',
-            title:'Parties',
-            payload:'Parties'
-          }
-        ]
-      }
-    }
+            type: 'postback',
+            title: 'Parties',
+            payload: 'Parties',
+          },
+        ],
+      },
+    },
   },
 
 
   Parties: {
 
-        text:'Pick a party you\'re interested in.',
-        quick_replies:[
-          {
-            content_type:'text',
-            title:'Conservative',
-            payload:'party_votes'
-          },
-          {
-            content_type: 'text',
-            title:'Labour',
-            payload:'party_votes'
-          },
-          {
-            content_type: 'text',
-            title:'Lib Dem',
-            payload:'party_votes'
-          },
-          {
-            content_type: 'text',
-            title:'UKIP',
-            payload:'party_votes'
-          }
-        ]
+    text: 'Pick a party you\'re interested in.',
+    quick_replies: [
+      {
+        content_type: 'text',
+        title: 'Conservative',
+        payload: 'party_votes',
+      },
+      {
+        content_type: 'text',
+        title: 'Labour',
+        payload: 'party_votes',
+      },
+      {
+        content_type: 'text',
+        title: 'Lib Dem',
+        payload: 'party_votes',
+      },
+      {
+        content_type: 'text',
+        title: 'UKIP',
+        payload: 'party_votes',
+      },
+    ],
 
 
   },
 
   party_votes: {
 
-        text:'Let\'s see how they voted on:',
-        quick_replies:[
-          {
-            content_type:'text',
-            title:'Brexit',
-            payload:'Brexit'
-          },
-          {
-            content_type:'text',
-            title:'Tuition fees',
-            payload:'Tuition fees'
-          }
-        ]
-  }
-}
+    text: 'Let\'s see how they voted on:',
+    quick_replies: [
+      {
+        content_type: 'text',
+        title: 'Brexit',
+        payload: 'Brexit',
+      },
+      {
+        content_type: 'text',
+        title: 'Tuition fees',
+        payload: 'Tuition fees',
+      },
+    ],
+  },
+};
 
 
-
-
-
-
-  module.exports = answer_objects;
+module.exports = answer_objects;
--- src/helper_functions/findLocalReply.js
+++ src/helper_functions/findLocalReply.js
@@ -1,19 +1,22 @@
 const answer_objects = require('./answer_objects');
 const sendToFB = require('./sendToFB');
+const get = require('./../database/get_data');
 
 module.exports = (senderID, intent, contexts) => {
-  for (let key in answer_objects) {
-
-  if (key === intent) {
-    console.log(key);
-    console.log('found intent=', intent);
-  const messageData = {
-    recipient: {
-      id: senderID,
-    },
-    message: answer_objects[key],
-  };
-  sendToFB(messageData);
-}
-}
+  const firstName = get.firstName(senderID, (err) => {
+    if (err) return err;
+  });
+  for (const key in answer_objects) {
+    if (key === intent) {
+      console.log(key);
+      console.log('found intent=', intent);
+      const messageData = {
+        recipient: {
+          id: senderID,
+        },
+        message: answer_objects[key],
+      };
+      sendToFB(messageData);
+    }
+  }
 };
