040feb81607162040648959e444abcef1fdca79e
ConchobarMacNessa <alice.binghamcarr@gmail.com>
2017-05-05 17:30:35 +0100

    more console logs relates #61

--- src/helper_functions/getFacebookName.js
+++ src/helper_functions/getFacebookName.js
@@ -4,24 +4,27 @@ const post = require('./../database/db_post');
 
 function getFacebookName(facebookId) {
   const url = `https://graph.facebook.com/v2.6/${facebookId}?access_token=${process.env.PAGE_ACCESS_TOKEN}`;
+  console.log(url);
   request(url, (err, res, body) => {
+    console.log('Im inside request');
     if (err) {
       console.log(err);
+    } else {
+      const parsedBody = JSON.parse(body);
+      // note, this also returns location and gender, in case we need these in the future
+      console.log(parsedBody);
+      const userDetails = {};
+      userDetails.firstname = parsedBody.first_name;
+      userDetails.lastname = parsedBody.last_name;
+      userDetails.facebook_id = Number(facebookId);
+      console.log('userDetails are ', userDetails);
+      // pushes to the database
+      post.userDetails(userDetails, (error) => {
+        if (error) {
+          console.log(error);
+        }
+      });
     }
-    const parsedBody = JSON.parse(body);
-    // note, this also returns location and gender, in case we need these in the future
-
-    const userDetails = {};
-    userDetails.firstname = parsedBody.first_name;
-    userDetails.lastname = parsedBody.last_name;
-    userDetails.facebook_id = Number(facebookId);
-    console.log('userDetails are ', userDetails);
-    // pushes to the database
-    post.userDetails(userDetails, (error) => {
-      if (error) {
-        console.log(error);
-      }
-    });
   });
 }
 
