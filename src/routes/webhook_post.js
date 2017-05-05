e5849b32360fda58b72c622d8ad4e31b414d95c9
ConchobarMacNessa <alice.binghamcarr@gmail.com>
2017-05-05 17:48:10 +0100

    more console logs relates #61

--- src/routes/webhook_post.js
+++ src/routes/webhook_post.js
@@ -4,9 +4,10 @@ const answer_objects = require('../helper_functions/answer_objects.js');
 const express = require('express');
 const findLocalReply = require('../helper_functions/findLocalReply.js');
 const getFacebookName = require('./../helper_functions/getFacebookName');
-
+const request = require('request');
 
 const app = express.Router();
+require('env2')('./config.env');
 
 // Listen for messages from user
 
@@ -26,6 +27,16 @@ module.exports = [
             checkAPIAI(event);
           } else if (event.postback && event.postback.payload) {
             console.log('payload is ', event.postback.payload);
+
+            const url = `https://graph.facebook.com/v2.6/${event.sender.id}?access_token=${process.env.PAGE_ACCESS_TOKEN}`;
+            request(url, (err, res, body) => {
+              console.log('Im inside request');
+              if (err) {
+                console.log(err);
+              }
+              const parsedBody = JSON.parse(body);
+              console.log(parsedBody);
+            });
             getFacebookName(event.sender.id);
             findLocalReply(event.sender.id, event.postback.payload);
           } else {
