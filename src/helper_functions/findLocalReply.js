60ce94f9989ad2375ab17ffcff1a07633e0176a4
ConchobarMacNessa <alice.binghamcarr@gmail.com>
2017-05-05 17:02:34 +0100

    this might work relates #31

--- src/helper_functions/findLocalReply.js
+++ src/helper_functions/findLocalReply.js
@@ -5,21 +5,23 @@ const get = require('./../database/get_data');
 
 module.exports = (senderID, intent, contexts) => {
   const firstName = get.firstName(senderID, (err) => {
-    if (err) return err;
-  });
-  console.log('firstname is ', firstName);
-  const answer_objects = constructAnswers(firstName);
-  for (const key in answer_objects) {
-    if (key === intent) {
-      // console.log(key);
-      // console.log('found intent=', intent);
-      const messageData = {
-        recipient: {
-          id: senderID,
-        },
-        message: answer_objects[key],
-      };
-      sendToFB(messageData);
+    if (err) {
+      return err;
     }
-  }
+    console.log('firstname is ', firstName);
+    const answer_objects = constructAnswers(firstName);
+    for (const key in answer_objects) {
+      if (key === intent) {
+          // console.log(key);
+          // console.log('found intent=', intent);
+        const messageData = {
+          recipient: {
+            id: senderID,
+          },
+          message: answer_objects[key],
+        };
+        sendToFB(messageData);
+      }
+    }
+  });
 };
