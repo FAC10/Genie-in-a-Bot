f937af45ec6fe3ee523609241fd5d68a4ca57536
Maja Kudlicka <maja.kudlicka@gmail.com>
2017-05-08 11:36:19 +0100

    modularised findLocalReply function
    relates to #66

---
+++ src/helper_functions/testfunction.js
@@ -0,0 +1,5 @@
+const multiply = function(a, b) {
+  return a*b;
+}
+
+module.exports = multiply;
--- tests/tests.js
+++ tests/tests.js
@@ -1,4 +1,10 @@
 const test = require('tape');
+var request = require('supertest');
+var routes = require('../src/routes/home.js');
+var app = require('../src/webhook.js');
+var multiply = require('../src/helper_functions/testfunction.js');
+var constructLocal = require('../src/helper_functions/findLocalReply.js');
+var constructAnswers = require('../src/helper_functions/answer_objects.js');
 // const server = require('../webhook.js');
 
 test('Passing test for travis', (t) => {
@@ -6,7 +12,73 @@ test('Passing test for travis', (t) => {
   t.end();
 });
 
-// test.onFinish(() => process.exit(0));
+test('Multiply funtion works', (t) => {
+  t.equal(multiply(2, 4), 8, '2 times 4 equals 8');
+  t.end();
+
+});
+
+
+test('constructLocal function returns expected result', (t) => {
+  const answer_objects = constructAnswers('Maja');
+  const testObject = constructLocal.constructLocal(1, 'Parties');
+  console.log(answer_objects);
+  console.log(testObject);
+  var expectedId = 1;
+  var actualId = testObject.recipient.id;
+  var actualMessage = testObject.message;
+  var expectedMessage = {
+
+    text: 'Pick a party you\'re interested in.',
+    quick_replies: [
+      {
+        content_type: 'text',
+        title: 'Conservative',
+        payload: 'party_votes',
+      },
+      {
+        content_type: 'text',
+        title: 'Labour',
+        payload: 'party_votes',
+      },
+      {
+        content_type: 'text',
+        title: 'Lib Dem',
+        payload: 'party_votes',
+      },
+      {
+        content_type: 'text',
+        title: 'UKIP',
+        payload: 'party_votes',
+      },
+    ],
+
+
+  }
+
+  t.equal(actualId, expectedId, 'constructLocal function returns an object with the correct sender id');
+  t.deepEqual(actualMessage, expectedMessage , 'constructLocal function returns an object with the correct message object')
+  t.end();
+
+})
+
+
+
+// test('GET home', function (assert) {
+//   request(routes)
+//     .get('/')
+//     .expect(200)
+//     .expect('Content-Type', /html/)
+//     .end(function (err, res) {
+//       var expectedThings = 'I should be working';
+//       var actualThings = res.body;
+//
+//       assert.error(err, 'No error');
+//       assert.same(actualThings, expectedThings, 'Retrieve list of things');
+//       assert.end();
+//     });
+// });
+
 
 // const routes = [{
 //   method: 'GET',
