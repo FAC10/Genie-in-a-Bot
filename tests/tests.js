e0b34793c79d647c32ee68efd20ac5ecfb2426cb
Maja Kudlicka <maja.kudlicka@gmail.com>
2017-05-08 12:20:01 +0100

    added more tests for constructLocal funtion

--- tests/tests.js
+++ tests/tests.js
@@ -1,10 +1,10 @@
 const test = require('tape');
-var request = require('supertest');
-var routes = require('../src/routes/home.js');
-var app = require('../src/webhook.js');
-var multiply = require('../src/helper_functions/testfunction.js');
-var constructLocal = require('../src/helper_functions/findLocalReply.js');
-var constructAnswers = require('../src/helper_functions/answer_objects.js');
+const request = require('supertest');
+const routes = require('../src/routes/home.js');
+const app = require('../src/webhook.js');
+const multiply = require('../src/helper_functions/testfunction.js');
+const constructLocal = require('../src/helper_functions/findLocalReply.js');
+const constructAnswers = require('../src/helper_functions/answer_objects.js');
 // const server = require('../webhook.js');
 
 test('Passing test for travis', (t) => {
@@ -12,22 +12,15 @@ test('Passing test for travis', (t) => {
   t.end();
 });
 
-test('Multiply funtion works', (t) => {
-  t.equal(multiply(2, 4), 8, '2 times 4 equals 8');
-  t.end();
 
-});
-
-
-test('constructLocal function returns expected result', (t) => {
+test('constructLocal function returns expected result for Parties intent', (t) => {
   const answer_objects = constructAnswers('Maja');
-  const testObject = constructLocal.constructLocal(1, 'Parties');
-  console.log(answer_objects);
-  console.log(testObject);
-  var expectedId = 1;
-  var actualId = testObject.recipient.id;
-  var actualMessage = testObject.message;
-  var expectedMessage = {
+  const testObject = constructLocal.constructLocal(1, 'Parties', answer_objects);
+  const expectedId = 1;
+  const actualId = testObject.recipient.id;
+
+  const actualMessage = testObject.message;
+  const expectedMessage = {
 
     text: 'Pick a party you\'re interested in.',
     quick_replies: [
@@ -54,16 +47,49 @@ test('constructLocal function returns expected result', (t) => {
     ],
 
 
-  }
+  };
 
-  t.equal(actualId, expectedId, 'constructLocal function returns an object with the correct sender id');
-  t.deepEqual(actualMessage, expectedMessage , 'constructLocal function returns an object with the correct message object')
+  t.equal(actualId, expectedId, 'constructLocal function returns an object with the correct sender id for Parties intent');
+  t.deepEqual(actualMessage, expectedMessage, 'constructLocal function returns an object with the correct message object for Parties intent');
   t.end();
-
-})
-
+});
 
 
+test('constructLocal function returns expected result for FACEBOOK_WELCOME intent', (t) => {
+  const answer_objects = constructAnswers('Maja');
+  const testObject = constructLocal.constructLocal(2, 'FACEBOOK_WELCOME', answer_objects);
+  const expectedGreeting = 'Hey Maja, I\'m your personal assistant in the run up to the General Elections! I can help you find out about the candidates standing in your area, what the parties are offering and more!';
+  const actualGreeting = testObject.message.attachment.payload.text;
+  const expectedId = 2;
+  const actualId = testObject.recipient.id;
+  const actualMessage = testObject.message;
+  const expectedMessage = {
+    attachment: {
+      type: 'template',
+      payload: {
+        template_type: 'button',
+        text: 'Hey Maja, I\'m your personal assistant in the run up to the General Elections! I can help you find out about the candidates standing in your area, what the parties are offering and more!',
+        buttons: [
+          {
+            type: 'postback',
+            title: 'Candidates',
+            payload: 'Candidates',
+          },
+          {
+            type: 'postback',
+            title: 'Parties',
+            payload: 'Parties',
+          },
+        ],
+      },
+    },
+  };
+
+  t.equal(actualGreeting, expectedGreeting, 'constructLocal function returns the correct greeting with the correct name for FACEBOOK_WELCOME intent');
+  t.equal(actualId, expectedId, 'constructLocal function returns an object with the correct sender id for FACEBOOK_WELCOME intent');
+  t.deepEqual(actualMessage, expectedMessage, 'constructLocal function returns an object with the correct message object for FACEBOOK_WELCOME intent');
+  t.end();
+});
 // test('GET home', function (assert) {
 //   request(routes)
 //     .get('/')
