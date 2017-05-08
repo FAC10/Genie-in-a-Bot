5d344f9fcc3c599913fe47cd731003ec85ed19b3
Maja Kudlicka <maja.kudlicka@gmail.com>
2017-05-08 15:52:28 +0100

    console logging location message object

--- src/routes/webhook_post.js
+++ src/routes/webhook_post.js
@@ -21,7 +21,7 @@ module.exports = [
 
         // Iterate over each messaging event
         entry.messaging.forEach((event) => {
-          console.log('Post code, hopefully?? ', JSON.stringify(event));
+          console.log('Post code, hopefully?? ', JSON.stringify(event.message.attachments[0]));
           if (event.message) {
             checkAPIAI(event);
           } else if (event.postback && event.postback.payload) {
@@ -44,3 +44,14 @@ module.exports = [
     }
   }),
 ];
+
+// {"mid":"mid.$cAACVbcqAv_NiBdGE4Vb1Iw7jHE0k",
+// "seq":134094,
+// "attachments":[
+//   {"title":"Philippa'sLocation",
+//   "url":"https://l.facebook.com/l.php?u=https%3A%2F%2Fwww.bing.com%2Fmaps%2Fdefault.aspx%3Fv%3D2%26pc%3DFACEBK%26mid%3D8100%26where1%3D51.529487%252C%2B-0.0423492%26FORM%3DFBKPL1%26mkt%3Den-US&h=ATPxu2eGlhkGKN7JvEQd1uCytjsk51-1aNqgfcnYK8i4YKYjQAVeZDuOoWBy_3UR4Sk5tlRFPxRIXC_b-E4ZiJtesVdkFpAhJKX3SF3Hx9Nn&s=1&enc=AZPAtqERgyPy0EE4XaJe9MV09jtmognjLxPBdLfvQo9W4_mciUnWbnkNBNDwtY-bRbgy03K5tMSkqYwyjq9LGdP2",
+//   "type":"location",
+//   "payload":{
+//     "coordinates":{
+//       "lat":51.529487,"long":-0.0423492}
+//     }}]}
