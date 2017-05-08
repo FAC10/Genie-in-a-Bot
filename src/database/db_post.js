edb412a045f1a5f12d46039d9bc639ab0f320d53
ConchobarMacNessa <alice.binghamcarr@gmail.com>
2017-05-08 10:28:48 +0100

    added console log relates #59

--- src/database/db_post.js
+++ src/database/db_post.js
@@ -6,7 +6,7 @@ post.userDetails = (userDetails, callback) => {
   const checkIfUserExists = 'SELECT facebook_id FROM users WHERE facebook_id = $1;';
   connect.query(checkIfUserExists, [userDetails.facebook_id], (err, user) =>{
     if (err) => {
-      if (err) { return callback(err); }
+      if (err) { console.log(err); }
 
       console.log('user.rows', user.rows);
       console.log('user.rows[0]', user.rows[0]);
