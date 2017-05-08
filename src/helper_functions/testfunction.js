fc5693bf3644791197bdae6de616638f2115f600
Maja Kudlicka <maja.kudlicka@gmail.com>
2017-05-08 15:19:57 +0100

    added statement to end tests

--- src/helper_functions/testfunction.js
+++
@@ -1,5 +0,0 @@
-const multiply = function(a, b) {
-  return a*b;
-}
-
-module.exports = multiply;
--- tests/tests.js
+++ tests/tests.js
@@ -2,18 +2,19 @@ const test = require('tape');
 const request = require('supertest');
 const routes = require('../src/routes/home.js');
 const app = require('../src/webhook.js');
-const multiply = require('../src/helper_functions/testfunction.js');
 const constructLocal = require('../src/helper_functions/findLocalReply.js');
 const constructAnswers = require('../src/helper_functions/answer_objects.js');
 // const server = require('../webhook.js');
 
 test('Passing test for travis', (t) => {
+  t.plan(1);
   t.equal(1, 1, '1 is equal to 1');
   t.end();
 });
 
 
 test('constructLocal function returns expected result for Parties intent', (t) => {
+  t.plan(2);
   const answer_objects = constructAnswers('Maja');
   const testObject = constructLocal.constructLocal(1, 'Parties', answer_objects);
   const expectedId = 1;
@@ -56,6 +57,7 @@ test('constructLocal function returns expected result for Parties intent', (t) =
 
 
 test('constructLocal function returns expected result for FACEBOOK_WELCOME intent', (t) => {
+  t.plan(3);
   const answer_objects = constructAnswers('Maja');
   const testObject = constructLocal.constructLocal(2, 'FACEBOOK_WELCOME', answer_objects);
   const expectedGreeting = 'Hey Maja, I\'m your personal assistant in the run up to the General Elections! I can help you find out about the candidates standing in your area, what the parties are offering and more!';
@@ -90,47 +92,6 @@ test('constructLocal function returns expected result for FACEBOOK_WELCOME inten
   t.deepEqual(actualMessage, expectedMessage, 'constructLocal function returns an object with the correct message object for FACEBOOK_WELCOME intent');
   t.end();
 });
-// test('GET home', function (assert) {
-//   request(routes)
-//     .get('/')
-//     .expect(200)
-//     .expect('Content-Type', /html/)
-//     .end(function (err, res) {
-//       var expectedThings = 'I should be working';
-//       var actualThings = res.body;
-//
-//       assert.error(err, 'No error');
-//       assert.same(actualThings, expectedThings, 'Retrieve list of things');
-//       assert.end();
-//     });
-// });
 
 
-// const routes = [{
-//   method: 'GET',
-//   url: '/',
-//   expectedStatusCode: 200,
-// }, {
-//   method: 'GET',
-//   url: '/webhook',
-//   expectedStatusCode: 200,
-// }, {
-//   method: 'POST',
-//   url: '/webhook',
-//   expectedStatusCode: 200,
-// }];
-//
-//
-// routes.forEach((route) => {
-//   test(`check ${route.url} route`, (t) => {
-//     const { method, url, expectedStatusCode } = route;
-//     const options = {
-//       method,
-//       url,
-//     };
-//     server.inject(options, (res) => {
-//       t.equal(res.status, expectedStatusCode, `Should return statuscode of ${expectedStatusCode}`);
-//       t.end();
-//     });
-//   });
-// });
+test.onFinish(() => process.exit(0));
