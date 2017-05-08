00b632d69b43194f5672650687bab41eb6d347b5
ConchobarMacNessa <alice.binghamcarr@gmail.com>
2017-05-08 10:26:38 +0100

    add logic for db conflicts relates #59

--- src/database/db_post.js
+++ src/database/db_post.js
@@ -3,14 +3,26 @@ const connect = require('./db_connect');
 const post = {};
 
 post.userDetails = (userDetails, callback) => {
-  connect.query('INSERT INTO users (firstname, lastname, facebook_id) VALUES ($1, $2, $3)', [userDetails.firstname, userDetails.lastname, userDetails.facebook_id],
-  (err, res) => {
-    if (err) {
-      console.log(err);
-      return callback(err);
+  const checkIfUserExists = 'SELECT facebook_id FROM users WHERE facebook_id = $1;';
+  connect.query(checkIfUserExists, [userDetails.facebook_id], (err, user) =>{
+    if (err) => {
+      if (err) { return callback(err); }
+
+      console.log('user.rows', user.rows);
+      console.log('user.rows[0]', user.rows[0]);
+
     }
-    callback(null, res);
-  });
+  })
+
+
+  // connect.query('INSERT INTO users (firstname, lastname, facebook_id) VALUES ($1, $2, $3)', [userDetails.firstname, userDetails.lastname, userDetails.facebook_id],
+  // (err, res) => {
+  //   if (err) {
+  //     console.log(err);
+  //     return callback(err);
+  //   }
+  //   callback(null, res);
+  // });
 };
 
 post.userPostcode = (userPostcode, callback) => {
