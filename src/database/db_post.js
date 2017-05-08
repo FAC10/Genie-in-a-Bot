b0255e0a3aba7055e999cd3fca352d08f15cdb6a
ConchobarMacNessa <alice.binghamcarr@gmail.com>
2017-05-08 10:49:38 +0100

    add logic to push user to db relates #59

--- src/database/db_post.js
+++ src/database/db_post.js
@@ -6,19 +6,20 @@ post.userDetails = (userDetails, callback) => {
   const checkIfUserExists = 'SELECT facebook_id FROM users WHERE facebook_id = $1;';
   connect.query(checkIfUserExists, [userDetails.facebook_id], (err, user) => {
     if (err) { return callback(err); }
-    console.log(user);
-    console.log(user.rows[0].facebook_id);
+
+    if (!user.rows[0]) {
+      connect.query('INSERT INTO users (firstname, lastname, facebook_id) VALUES ($1, $2, $3)', [userDetails.firstname, userDetails.lastname, userDetails.facebook_id],
+      (err, res) => {
+        if (err) {
+          console.log(err);
+          return callback(err);
+        }
+        callback(null, res);
+      });
+    } else {
+      console.log('user already in db');
+    }
   });
-
-
-  // connect.query('INSERT INTO users (firstname, lastname, facebook_id) VALUES ($1, $2, $3)', [userDetails.firstname, userDetails.lastname, userDetails.facebook_id],
-  // (err, res) => {
-  //   if (err) {
-  //     console.log(err);
-  //     return callback(err);
-  //   }
-  //   callback(null, res);
-  // });
 };
 
 post.userPostcode = (userPostcode, callback) => {
