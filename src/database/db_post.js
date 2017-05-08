82650877854ad01de3543110c6c597ab7c9e6736
ConchobarMacNessa <alice.binghamcarr@gmail.com>
2017-05-08 10:36:38 +0100

    removing silly nested if statments relates #59

--- src/database/db_post.js
+++ src/database/db_post.js
@@ -5,12 +5,10 @@ const post = {};
 post.userDetails = (userDetails, callback) => {
   const checkIfUserExists = 'SELECT facebook_id FROM users WHERE facebook_id = $1;';
   connect.query(checkIfUserExists, [userDetails.facebook_id], (err, user) => {
-    if (err) {
-      if (err) { console.log(err); }
+    if (err) { return callback(err); }
 
-      console.log('user.rows', user.rows);
-      console.log('user.rows[0]', user.rows[0]);
-    }
+    console.log('user.rows', user.rows);
+    console.log('user.rows[0]', user.rows[0]);
   });
 
 
