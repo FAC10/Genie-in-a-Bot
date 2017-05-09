f5243d46836bf9503fed5a947fc163400b45e12c
pbywater <philippabywater@gmail.com>
2017-05-09 12:38:14 +0100

    changing const to let on partyVotesObj
    relates to #67

--- src/helper_functions/answer_objects.js
+++ src/helper_functions/answer_objects.js
@@ -1,6 +1,6 @@
 const extractContexts = require('./extractContexts');
 const get = require('../database/get_data');
-const partyVotesObj = { party: null, issue: null, inFavour: null, against: null, turnout: null };
+let partyVotesObj = { party: null, issue: null, inFavour: null, against: null, turnout: null };
 
 
 function constructAnswers(firstName, contexts, intent) {
