e86a5b49b5fa960b2f437514e5e6aac25a9346c0
ConchobarMacNessa <alice.binghamcarr@gmail.com>
2017-05-09 16:09:00 +0100

    updated dbs with correct queries realtes #67

--- src/helper_functions/extractContexts.js
+++ src/helper_functions/extractContexts.js
@@ -2,7 +2,6 @@ function extractContexts(contexts, intent) {
   if (intent === 'brexit') {
     const parameter = contexts[0].parameters.Parties;
     const context = contexts[1].name;
-    console.log('db query is', parameter + context);
     return parameter + context;
   }
 }
