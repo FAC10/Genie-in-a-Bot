e35c228169aaf1aac6bde75b20f98a908f633515
ConchobarMacNessa <alice.binghamcarr@gmail.com>
2017-05-09 16:50:11 +0100

    added console log relates #78

--- src/helper_functions/checkAPIAI.js
+++ src/helper_functions/checkAPIAI.js
@@ -31,7 +31,7 @@ module.exports = (event) => {
       const intent = response.result.metadata.intentName;
       const contexts = response.result.contexts;
       const resolvedQuery = response.result.resolvedQuery;
-
+      console.log('contexts is ', contexts);
       if (contexts === 'party_votes') {
         contexts = resolvedQuery;
       }
