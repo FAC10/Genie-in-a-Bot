2c20fa5fb1f6a41bffabd3e2ef7ab2477fb1bd23
pbywater <philippabywater@gmail.com>
2017-05-09 16:17:12 +0100

    adding more console logs
    relates to #76

--- src/helper_functions/constructRemoteReply.js
+++ src/helper_functions/constructRemoteReply.js
@@ -2,6 +2,7 @@ const sendToFB = require('./sendToFB');
 const welcome_message = require('./answer_objects.js');
 
 module.exports = (senderID, responseText) => {
+  console.log('responseText in constructRemoteReply is ', responseText);
   const messageData = {
     recipient: {
       id: senderID,
