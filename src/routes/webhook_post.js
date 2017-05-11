b4266e3ea2553d6e8f8f7289dac05185b58865e2
pbywater <philippabywater@gmail.com>
2017-05-11 16:38:07 +0100

    fixing issue
    relates to #101

--- src/routes/webhook_post.js
+++ src/routes/webhook_post.js
@@ -25,55 +25,63 @@ module.exports = [
         // Iterate over each messaging event
         entry.messaging.forEach((event) => {
           console.log(event);
+          if (event.message) {
+            if (event.message.attachments) {
+              if (event.message.attachments[0].payload.coordinates) {
+                const lat = JSON.stringify(event.message.attachments[0].payload.coordinates.lat);
+                const long = JSON.stringify(event.message.attachments[0].payload.coordinates.long);
 
-          getPostcode(lat, long, (postCode, constituency) => {
-            const userConstituency = { constituency, facebook_id: event.sender.id };
-            const userPostcode = { postcode: postCode, facebook_id: event.sender.id };
-            post.userPostcode(userPostcode, (err, result) => {
-              if (err) {
-                console.log(err);
+                getPostcode(lat, long, (postCode, constituency) => {
+                  const userConstituency = { constituency, facebook_id: event.sender.id };
+                  const userPostcode = { postcode: postCode, facebook_id: event.sender.id };
+                  post.userPostcode(userPostcode, (err, result) => {
+                    if (err) {
+                      console.log(err);
+                    }
+                  });
+                  post.userConstituency(userConstituency, (err, result) => {
+                    if (err) {
+                      return err;
+                    }
+                  });
+                  findLocalReply.findLocalReply(event.sender.id, 'runningCandidates');
+                });
               }
+            }
+          }
+
+          if (event.message) {
+            checkAPIAI(event);
+          } else if (event.postback && event.postback.payload) {
+            console.log(event.postback.payload);
+            if (event.postback.payload === 'About this bot') {
+              const messageData = {
+                recipient: {
+                  id: event.sender.id,
+                },
+                message: {
+                  text: 'This bot has been created as a study project by students at Founders & Coders. Find us on github!: https://github.com/FAC10/MPBots',
+                },
+              };
+              sendToFB(messageData);
+            }
+          } else if (event.postback.payload === 'Report the problem') {
+            const messageData = {
+              recipient: {
+                id: event.sender.id,
+              },
+              message: {
+                text: 'We are sorry to hear that you have encoutered a problem! You can contact us at GenieInTheBot@outlook.com - we try to answer all queries and address all problems. We welcome your feedback! ',
+              },
+            };
+            sendToFB(messageData);
+          } else {
+            getFacebookName(event.sender.id, () => {
+              findLocalReply.findLocalReply(event.sender.id, event.postback.payload);
             });
-            post.userConstituency(userConstituency, (err, result) => {
-              if (err) {
-                return err;
-              }
-            });
-            findLocalReply.findLocalReply(event.sender.id, 'runningCandidates');
-          });
+          }
         });
       });
-
-      if (event.message) {
-        checkAPIAI(event);
-      } else if (event.postback && event.postback.payload) {
-        console.log(event.postback.payload);
-        if (event.postback.payload === 'About this bot') {
-          const messageData = {
-            recipient: {
-              id: event.sender.id,
-            },
-            message: {
-              text: 'This bot has been created as a study project by students at Founders & Coders. Find us on github!: https://github.com/FAC10/MPBots',
-            },
-          };
-        }
-        sendToFB(messageData);
-      } else if (event.postback.payload === 'Report the problem') {
-        const messageData = {
-          recipient: {
-            id: event.sender.id,
-          },
-          message: {
-            text: 'We are sorry to hear that you have encoutered a problem! You can contact us at GenieInTheBot@outlook.com - we try to answer all queries and address all problems. We welcome your feedback! ',
-          },
-        };
-        sendToFB(messageData);
-      } else {
-        getFacebookName(event.sender.id, () => {
-          findLocalReply.findLocalReply(event.sender.id, event.postback.payload);
-        });
-      }
 
 
       // Assume all went well.
