759016aea9d581860c04d093d5b1bc8f64aab02e
ConchobarMacNessa <alice.binghamcarr@gmail.com>
2017-05-11 12:53:47 +0100

    added in candidates relates #71

--- src/helper_functions/answer_objects.js
+++ src/helper_functions/answer_objects.js
@@ -31,20 +31,7 @@ function construct(partyVotesObj, firstName, candidates) {
         type: 'template',
         payload: {
           template_type: 'generic',
-          elements: [ //added in square brackets
-          {
-            title: 'Rushanara Ali',
-            image_url: 'https://candidates.democracyclub.org.uk/media/images/5535cf82367dc5383f552cef.png',
-            subtitle: 'Labour Party',
-            buttons:
-              [ { type: 'postback',
-                  title: 'Recent tweets',
-                  payload: 'Recent tweets' },
-                { type: 'postback',
-                  title: 'Recent mentions',
-                  payload: 'Recent mentions' } ]
-                },
-              ]
+          elements: [ candidates ]
         },
       },
     },
