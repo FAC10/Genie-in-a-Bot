bfe8abb742090f77ed1a3a167e279f96d27874d5
pbywater <philippabywater@gmail.com>
2017-05-12 12:02:44 +0100

    console logging
    relates to #110

--- src/helper_functions/getTweets.js
+++ src/helper_functions/getTweets.js
@@ -2,20 +2,18 @@ const Twitter = require('twitter');
 require('env2')('./config.env');
 const constructRemoteReply = require('./constructRemoteReply.js');
 
-function getTweets() {
+function getTweets(senderID, username) {
   const client = new Twitter({
     consumer_key: process.env.TWITTER_CONSUMER_KEY,
     consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
     bearer_token: process.env.TWITTER_BEARER_TOKEN,
   });
 
-  function getUserTweets(senderID, username) {
-    client.get('search/tweets', { q: `from:${username}` }, (error, tweets, response) => {
-      console.log(tweets);
-      const messageOne = tweets.statuses[0].text;
-      constructRemoteReply(senderID, messageOne);
-    });
-  }
+  client.get('search/tweets', { q: `from:${username}` }, (error, tweets, response) => {
+    console.log(tweets);
+    const messageOne = tweets.statuses[0].text;
+    constructRemoteReply(senderID, messageOne);
+  });
 }
 
 module.exports = getTweets;
