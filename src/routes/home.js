52da8ddd76c1962a68a76bb8dce0885d1221f46f
ConchobarMacNessa <alice.binghamcarr@gmail.com>
2017-05-12 12:49:45 +0100

    updated heroku homepage with info on how to talk to bot relates #113

--- src/routes/home.js
+++ src/routes/home.js
@@ -5,7 +5,8 @@ const app = express.Router();
 /* Home */
 module.exports = [
   app.get('/', (req, res) => {
-    res.send('I should be working');
+    const url = 'https://m.me/politicalgenie';
+    res.send(`Visit <a href=${url}>https://m.me/politicalgenie</a> to start talking to me`);
     res.status(200);
   }),
 ];
