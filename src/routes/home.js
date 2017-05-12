64ef8485d0f24b0cdbd594b49457746161ef4fb4
ConchobarMacNessa <alice.binghamcarr@gmail.com>
2017-05-12 12:56:27 +0100

    updated text on heroku slightly relates #113

--- src/routes/home.js
+++ src/routes/home.js
@@ -6,7 +6,7 @@ const app = express.Router();
 module.exports = [
   app.get('/', (req, res) => {
     const url = 'https://m.me/politicalgenie';
-    res.send(`Visit <a href=${url}>https://m.me/politicalgenie</a> to start talking to me`);
+    res.send(`Visit me <a href=${url}>on facebook</a> to start talking to me`);
     res.status(200);
   }),
 ];
