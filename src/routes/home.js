8a20a687c59323c78950cb6fbe2e5ab9e494728a
pbywater <philippabywater@gmail.com>
2017-05-12 11:20:04 +0100

    getting twitter access token
    relates to #110

--- src/routes/home.js
+++ src/routes/home.js
@@ -1,10 +1,21 @@
 const express = require('express');
-
+require('env2')('./config.env');
+const twitter_consumer_key = process.env.TWITTER_CONSUMER_KEY;
+const twitter_consumer_secret = process.env.TWITTER_CONSUMER_SECRET;
+const getBearerToken = require('get-twitter-bearer-token');
 const app = express.Router();
 
 /* Home */
 module.exports = [
   app.get('/', (req, res) => {
+    getBearerToken(twitter_consumer_key, twitter_consumer_secret, (err, res) => {
+      if (err) {
+        console.log(err);
+      } else {
+      // bearer token
+        console.log('twitter access token is ', res.body.access_token);
+      }
+    });
     res.send('I should be working');
     res.status(200);
   }),
--- src/routes/webhook_get.js
+++ src/routes/webhook_get.js
@@ -1,8 +1,5 @@
 const express = require('express');
 require('env2')('./config.env');
-const getBearerToken = require('get-twitter-bearer-token');
-const twitter_consumer_key = process.env.TWITTER_CONSUMER_KEY;
-const twitter_consumer_secret = process.env.TWITTER_CONSUMER_SECRET;
 
 const app = express.Router();
 
@@ -16,16 +13,6 @@ module.exports = [
     } else {
       console.error('Failed validation. Make sure the validation tokens match.');
       res.sendStatus(403);
-    }
-  }),
-
-
-  getBearerToken(twitter_consumer_key, twitter_consumer_secret, (err, res) => {
-    if (err) {
-    // handle error
-    } else {
-    // bearer token
-      console.log('twitter access token is ', res.body.access_token);
     }
   }),
 ];
