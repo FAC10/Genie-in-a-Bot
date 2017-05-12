32eca45d426af4492c02c765cdd838361ac188aa
pbywater <philippabywater@gmail.com>
2017-05-12 12:16:03 +0100

    attempting to add all three
    relates to #110

--- src/helper_functions/getTweets.js
+++ src/helper_functions/getTweets.js
@@ -10,14 +10,16 @@ function getTweets(senderID, username) {
   });
 
   client.get('search/tweets', { q: `from:${username}` }, (error, tweets, response) => {
-    console.log(tweets);
+    constructRemoteReply(senderID, `The last three tweets from ${username}:`);
     const time = (tweets.statuses[0].created_at).split(' ', 3).join(' ');
     const time2 = (tweets.statuses[1].created_at).split(' ', 3).join(' ');
-    console.log(time);
+    const time3 = (tweets.statuses[2].created_at).split(' ', 3).join(' ');
     const messageOne = `${username} tweeted this on ${time}: ${tweets.statuses[0].text}`;
     constructRemoteReply(senderID, messageOne);
     const messageTwo = `${username} tweeted this on ${time2}: ${tweets.statuses[1].text}`;
     constructRemoteReply(senderID, messageTwo);
+    const messageThree = `${username} tweeted this on ${time3}: ${tweets.statuses[2].text}`;
+    constructRemoteReply(senderID, messageThree);
   });
 }
 
