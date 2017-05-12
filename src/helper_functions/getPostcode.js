de5e79be67a7b4dc64a34616e27253303bcab956
ConchobarMacNessa <alice.binghamcarr@gmail.com>
2017-05-12 12:27:29 +0100

    console logging error code when overseas relates #121

--- src/helper_functions/getPostcode.js
+++ src/helper_functions/getPostcode.js
@@ -1,15 +1,14 @@
 const request = require('request');
 
 function getPostcode(latitude, longtitude, cb) {
-  console.log('latitude is', latitude);
-  console.log('longtitude is', longtitude);
   const url = `https://api.postcodes.io/postcodes?lon=${longtitude}&lat=${latitude}`;
   request(url, (err, res, body) => {
     if (err) {
       console.log(err);
     }
-
     const parsedBody = JSON.parse(body);
+    const overseasCode = parsedBody.result;
+    console.log('overseasCode is', overseasCode);
     const postCode = parsedBody.result[0].postcode;
     const constituency = parsedBody.result[0].parliamentary_constituency;
     cb(postCode, constituency);
