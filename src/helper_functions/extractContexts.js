86dda122f9fdf666ff150b76d6c452f012d4bc36
pbywater <philippabywater@gmail.com>
2017-05-13 18:35:50 +0100

    adding logic for if there are no contexts to compare
    relates to #133

--- src/helper_functions/extractContexts.js
+++ src/helper_functions/extractContexts.js
@@ -1,10 +1,15 @@
 const get = require('../database/get_data');
+const construct = require('./answer_objects');
 
 function extractContexts(senderID, intent, cb) {
   console.log('I am in extractContexts');
   get.party(senderID, (err, res) => {
     if (err) {
       return err;
+    }
+    if (!res) {
+      const placeholderVotingObj = { party: null, issue: null, inFavour: null, against: null, turnout: null };
+      construct(placeholderVotingObj, null, null, null, senderID, 'Parties', searchAnsObjects);
     }
     const partyKey = res + intent;
     console.log('I am sending partyKey of ', partyKey);
