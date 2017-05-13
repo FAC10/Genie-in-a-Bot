53334f0a10bfc7f3b3c12b8f65dcbb980f512570
pbywater <philippabywater@gmail.com>
2017-05-13 17:41:59 +0100

    testing smug emoji
    relates to #132

--- database_build/db_build.sql
+++ database_build/db_build.sql
@@ -34,7 +34,7 @@ CREATE TABLE jokes (
 
 INSERT INTO jokes (joke, image_url)
 VALUES
-('I''m not a big fan of political jokes. I have seen too many of them get elected...', '../public/assets/smug.png');
+('I''m not a big fan of political jokes. I have seen too many of them get elected...', 'http://imgur.com/a/I2Wel');
 
 
 INSERT INTO partyVotes (partyKey, party, issue, inFavour, against, turnout, swing, majority)
