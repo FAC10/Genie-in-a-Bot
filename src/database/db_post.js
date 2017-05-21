b05b56b6da378762ea20a1e723343f0429a2a4c0
pbywater <philippabywater@gmail.com>
2017-05-21 14:26:55 +0100

    adding queries to get data from manifestos in database
    relates to #159

--- src/database/db_post.js
+++ src/database/db_post.js
@@ -49,9 +49,10 @@ post.startContext = (context, facebook_id, callback) => {
 
 post.party = (contexts, facebook_id, callback) => {
   if (contexts !== 'Back to votes' && contexts !== 'Back to parties') {
+    console.log('party in post party is ', party);
     connect.query(`UPDATE users SET party = '{${contexts}}' WHERE facebook_id = ${facebook_id}`, (err, res) => {
       if (err) return callback(err);
-      callback(null, res);
+      return callback(null, res);
     });
   }
 };
