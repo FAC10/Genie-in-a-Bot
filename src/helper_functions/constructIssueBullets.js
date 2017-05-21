7e0019bf8e85b658be561c95381ea50b87fdfe4c
pbywater <philippabywater@gmail.com>
2017-05-21 15:04:40 +0100

    adding queries to get data from manifestos in database
    relates to #159

--- src/helper_functions/constructIssueBullets.js
+++ src/helper_functions/constructIssueBullets.js
@@ -47,7 +47,7 @@ function constructIssueBullets(facebookId, intent, party) {
         },
       ] };
     constructRemoteReply(facebookId, responseText);
-  } else if (party) {
+  } else if (party && lastIntent !== intent) {
     console.log('inside party in construct bullets');
     if (party.toLowerCase() === 'lib dem') {
       party = party.toLowerCase().split(' ').join('');
@@ -79,7 +79,7 @@ function constructIssueBullets(facebookId, intent, party) {
         constructRemoteReply(facebookId, responseText2);
       }
     });
-  } else if (lastIntent = intent) {
+  } else if (lastIntent === intent) {
     console.log('lastIntent = intent so info');
     get.manifestoData(count, intent, party, (err, maniData) => {
       console.log('inside get manifestoData');
